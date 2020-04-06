551c79cac987ebbfa5c5c7c2ed9c8568001c412a
Sydoruk, Kostya <Kostya.Sydoruk@3DSystems.Com>
2020-04-06 20:17:06 +0300

    test tool press T to switch, guy barfing is G

---
+++ Assets/CaseSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseSwitcher : MonoBehaviour
+{
+    public List<GameObject> gameObjects = new List<GameObject>(3);
+    private int currentIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            gameObjects[currentIndex].SetActive(false);
+            currentIndex++;
+            currentIndex = currentIndex % gameObjects.Count;
+            gameObjects[currentIndex].SetActive(true);
+        }
+    }
+}
